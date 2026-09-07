d9b79847dda60d5eb546757d22c38f961858e3fc
Vibeosys Candidate <candidate@example.com>
2026-09-07 16:42:06 +0530

    Fix pythonpath in setup_mysql.py script

--- scripts/setup_mysql.py
+++ scripts/setup_mysql.py
@@ -1,4 +1,10 @@
 import os
+import sys
+from pathlib import Path
+
+# Add project root directory to sys.path
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
 import pymysql
 from dotenv import load_dotenv
 
